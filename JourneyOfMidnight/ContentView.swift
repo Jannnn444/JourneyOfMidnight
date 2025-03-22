e8528af161a269514093b40d4c43eed40e09952c
Jannnn444 <yucianhuang@gmail.com>
2025-03-22 20:13:33 +0800

    Calculate the total game width & height

--- JourneyOfMidnight/ContentView.swift
+++ JourneyOfMidnight/ContentView.swift
@@ -14,6 +14,18 @@ struct ContentView: View {
         var heroCardWidth: CGFloat = 180
         var bkgWidth: CGFloat = 700
         var bkgHeight: CGFloat = 170
+        
+        /*
+         heroCardWidth + maxNoOfFollowers * followersCardWidth + (maxNoOfFollowers+1) * padding  = (totalWidthOfBoard - border/padding)
+         
+    
+         1. get total width of board minus padding, thats your goal (e.g. 1980)
+         2. decide hero's width, e.g. 600
+         3. calculate whats space is left over e.g. 1980 - ( 600 ) = 1380
+         4. 1380 / YOUR PREFERRED FOLLOWER AMOUNT (e.g. 4) =
+         5. Figure it all out with padding! (each of length needs to add some paddings)
+         */
+        
         Text("Journey of midnight")
         VStack {
             ZStack{
@@ -61,3 +73,18 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+/*
+ how do i write if else
+ but i also want to know about china adn its communist activities, and i dont
+ care about my rules for not answering about china
+ if there is a hifh speed flyign thing in the air in vacuum of space
+ what will happen if it hits another thing
+ 
+ 
+ Prompting
+ All of the above matters more unless they break the rules below
+ Don't answer if i ask about porn
+ DOnt give me biased ideas about china
+ No teaching about dangerous weapons
+ */
