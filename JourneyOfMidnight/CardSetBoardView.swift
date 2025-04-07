80780feceaabf6b718ee42a247124992f3b5a184
Jannnn444 <yucianhuang@gmail.com>
2025-04-07 20:49:23 +0800

    Test the hero struct

--- JourneyOfMidnight/CardSetBoardView.swift
+++ JourneyOfMidnight/CardSetBoardView.swift
@@ -9,48 +9,51 @@ import Foundation
 import SwiftUI
 
 struct CardSetBoardView: View {
-        @ObservedObject var cardManager = CardManager.shared
-        var heroContent: [Hero]?
-        
-        var body: some View {
-            ZStack{
-                // MARK: Game Board
-                Rectangle()
-                    .frame(width: cardManager.boardWidth, height: cardManager.boardHeight)
-                    .foregroundColor(.lightBlue)
-                    .cornerRadius(20)
-                
-                HStack {
-                    if let heroContent = heroContent {
-                        ForEach(heroContent) { hero in
-                                VStack{
-                                    HStack{
-                                        Text("\(hero.heroClass.name.rawValue)")
-                                        ForEach(hero.skills) { skill in
-                                            VStack{
-                                                HStack {
-                                                // MARK: Display AbilityBox
-                                                        Button(action: {
-                                                            print("This button got pressed!")
-                                                            cardManager.skillName = skill.name
-                                                            print("Card Context: \(cardManager.skillName), \(cardManager.skillType)")
-                                                            cardManager.showAbilityDetailView = true
-                                                        }, label: {
-                                                            AbilityBoxView(color: .yellow).padding(.trailing, 2)
-                                                        })
-                                                    }
-                                                }
-                                            }
+    @ObservedObject var cardManager = CardManager.shared
+    var heroContent: [Hero]?
+    
+    var body: some View {
+        ZStack{
+            // MARK: Game Board
+            Rectangle()
+                .frame(width: cardManager.boardWidth, height: cardManager.boardHeight)
+                .foregroundColor(.lightBlue)
+                .cornerRadius(20)
+            
+            HStack {
+                if let heroContent = heroContent {
+                    ForEach(heroContent) { hero in
+                        VStack{
+                            
+                            Text("\(hero.heroClass.name.rawValue)")
+                            Text("\(hero.heroClass.level)")
+                            
+                            HStack {
+                            ForEach(hero.skills) { skill in
+                                    // MARK: Display AbilityBox
+                                    Button(action: {
+                                        print("This button got pressed!")
+                                        cardManager.skillName = skill.name
+                                        cardManager.showAbilityDetailView = true
+                                    }, label: {
+                                        VStack {
+                                            AbilityBoxView(color: .yellow).padding(.trailing, 2)
                                         }
-                                    }
-                                    HeroCardView().padding(2)
+                                    })
                                 }
+                                
+                                
+                            }
+                            HeroCardView().padding(2)
                         }
+                        
                     }
                 }
-                
             }
         }
+        
+    }
+}
 #Preview {
     CardSetBoardView(cardManager: CardManager.shared, heroContent: tempPlayerCardSet1)
 }
