acff8fd2569ebb73400f36cb20b7cfe0af3556b7
Jannnn444 <yucianhuang@gmail.com>
2025-04-18 10:54:14 +0800

    Have the layers adjust

--- JourneyOfMidnight/HeroMainView.swift
+++ JourneyOfMidnight/HeroMainView.swift
@@ -19,36 +19,36 @@ struct HeroMainView: View {
             Color.clear
                 .contentShape(Rectangle())
                 .onTapGesture {
-//               showDetailSkillView = false
-//               selectedHero = nil
+                    //               showDetailSkillView = false
+                    //               selectedHero = nil
                 }
             // MARK: Top 1/2 Banner GameBd
-                switch eventState {
-                case .Game:
-                    Rectangle()
-                        .frame(width: 500, height: 350)
-                        .foregroundColor(.blue)
-                        .cornerRadius(20)
-                case .FortuneWheel:
-                    EmptyView()
-                case .GroceryShop:
-                    EmptyView()
-                case .Sleep:
-                    EmptyView()
-                case .Forest:
-                    EmptyView()
-                }
+            switch eventState {
+            case .Game:
+                Rectangle()
+                    .frame(width: 500, height: 350)
+                    .foregroundColor(.blue)
+                    .cornerRadius(20)
+            case .FortuneWheel:
+                EmptyView()
+            case .GroceryShop:
+                EmptyView()
+            case .Sleep:
+                EmptyView()
+            case .Forest:
+                EmptyView()
+            }
             
             VStack {
-            
+                
                 // MARK: - 2/2 B.Bounced Skill View
                 if selectedHero != nil {
                     VStack(alignment: .leading) {
                         PopupView{
                             Text("\(selectedHero!.heroClass.name.rawValue.capitalized)")
-                                    .foregroundStyle(.black)
-                                    .font(.headline)
-                                    .bold()
+                                .foregroundStyle(.black)
+                                .font(.headline)
+                                .bold()
                             ForEach(selectedHero!.skills) { skill in
                                 Text("\(skill.name): \(skill.power)")
                                     .foregroundStyle(.black)
@@ -91,20 +91,19 @@ struct HeroMainView: View {
                                         .frame(width: 100, height: 130)
                                         .foregroundColor(.yellow)
                                         .cornerRadius(10)
-                                        VStack() {
-                                            Text(hero.heroClass.name.rawValue.capitalized)
-                                                .font(.headline)
-                                                .padding()
-                                                .fontDesign(.monospaced)
-                                                .bold()
-                                        }
+                                    VStack() {
+                                        Text(hero.heroClass.name.rawValue.capitalized)
+                                            .font(.headline)
+                                            .padding()
+                                            .fontDesign(.monospaced)
+                                            .bold()
                                         
                                         if showDetailSkillView {
                                             // heres what we gotta do
                                             // makes the view clickable
                                             VStack(alignment: .leading) {
                                                 ForEach(hero.skills) { skill in
-                                                    // HAve the skills able to be buttons and expand to show more infos
+// Have the skills able to be buttons and expand to show more infos
                                                     Button(action: {
                                                         cardManager.showMoreDetail = true
                                                         selectedHero = hero
@@ -114,6 +113,8 @@ struct HeroMainView: View {
                                                 }
                                             }
                                         }
+                                    }
+                                    
                                 } // Zstack
                             }
                         }
@@ -122,7 +123,7 @@ struct HeroMainView: View {
                 } // selectedHero = nil >>> else view shows
             }
         }.padding([.top, .leading, .trailing])
-//       .ignoresSafeArea()
+        //       .ignoresSafeArea()
     }
 }
 
