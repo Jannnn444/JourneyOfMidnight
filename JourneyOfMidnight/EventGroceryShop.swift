e4feff21c8dc75aa1c24b11f0caf6bde048267a3
Jannnn444 <yucianhuang@gmail.com>
2025-04-21 18:11:52 +0800

    Add gray color on item card

--- JourneyOfMidnight/EventGroceryShop.swift
+++ JourneyOfMidnight/EventGroceryShop.swift
@@ -50,7 +50,7 @@ struct EventGroceryShop: View {
                     ZStack {
                         Rectangle()
                             .frame(width: 100, height: 130)
-                            .foregroundColor(.brown)
+                            .foregroundColor(.gray)
                             .cornerRadius(10)
                             .offset(x: 5)
                             .offset(y: 8)
