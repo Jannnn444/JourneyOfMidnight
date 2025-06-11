ea239d90ffb5f9c1c315158cd939f30e4722ea64
Jannnn444 <yucianhuang@gmail.com>
2025-06-11 16:34:26 +0800

    fix the shows selectItem into one single shows

--- JourneyOfMidnight/View/Events/EventVendorShop.swift
+++ JourneyOfMidnight/View/Events/EventVendorShop.swift
@@ -17,11 +17,6 @@ struct EventVendorShop: View {
     var body: some View {
         ZStack {
         // MARK: - Banner and Event title
-//            Rectangle()
-//                .frame(width: 500, height: 350)
-//                .foregroundColor(.indigo.opacity(0.8))
-//                .cornerRadius(20)
-            
             VStack {
                 HStack {
                     Image("vendor")
