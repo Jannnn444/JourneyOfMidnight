74518d373b404287c98bb7dc67c42ee443a6d0f9
Jannnn444 <yucianhuang@gmail.com>
2025-08-05 14:59:20 +0800

    Save progress on Hero object

--- JourneyOfMidnight/Manager/CardManager.swift
+++ JourneyOfMidnight/Manager/CardManager.swift
@@ -64,7 +64,7 @@ class CardManager: ObservableObject {
                 stats: Stats(health: 100, endurance: 500),
                 bag: [Item(name: "apple", intro: "Food", price: 20, size: .small)],
                 heroLoad: 2,
-                activeSkills: [Skill(name: "Rainy", power: 7, size: .small), Skill(name: "Meteor", power: 9, size: .small)],
+                activeSkills: [Skill(name: "Rainy", power: 7, size: .small), Skill(name: "Meteor", power: 9, size: .small)]
             ),
             
             // 2 - Hero !!
@@ -76,7 +76,7 @@ class CardManager: ObservableObject {
                 stats: Stats(health: 100, endurance: 500),
                 bag: [Item(name: "apple", intro: "Food", price: 20, size: .small)],
                 heroLoad: 4,
-                activeSkills: [Skill(name: "Rainy", power: 7, size: .large), ],
+                activeSkills: [Skill(name: "Rainy", power: 7, size: .large), ]
             ),
             
             // 3 - Follower
@@ -88,7 +88,7 @@ class CardManager: ObservableObject {
                 stats: Stats(health: 100, endurance: 500),
                 bag: [Item(name: "doggo", intro: "Sleepy pal", price: 10, size: .medium)],
                 heroLoad: 2,
-                activeSkills: [Skill(name: "Rainy", power: 7, size: .small), Skill(name: "WolveCry", power: 5, size: .small)],
+                activeSkills: [Skill(name: "Rainy", power: 7, size: .small), Skill(name: "WolveCry", power: 5, size: .small)]
             ),
             
             // 4 - Follower
@@ -108,21 +108,21 @@ class CardManager: ObservableObject {
             Hero(heroClass: HeroClass(name: .wizard, level: 10, life: 80), attributes: Attributes(Strength: 6, Intelligence: 3, Wisdom: 3, Agility: 3, Vitality: 3, Faith: 3, Charisma: 3), skills: [Skill(name: "meow", power: 4, size: .small)], items: [Item(name: "cat", intro: "pet", price: 500, size: .small)/*, Item(name: "staff", intro: "weapon")*/], stats: Stats(health: 100, endurance: 500),
                  bag: [Item(name: "apple", intro: "Food", price: 5, size: .small)],
                  heroLoad: 2,
-                 activeSkills: [Skill(name: "Rainy", power: 7,size: .small), Skill(name: "meow", power: 4, size: .small)],
+                 activeSkills: [Skill(name: "Rainy", power: 7,size: .small), Skill(name: "meow", power: 4, size: .small)]
                 ),
             
             // 2 - Hero !!
             Hero(heroClass: HeroClass(name: .templar, level: 11, life: 180), attributes: Attributes(Strength: 5, Intelligence: 10, Wisdom: 7, Agility: 7, Vitality: 7, Faith: 7, Charisma: 7), skills: [Skill(name: "Holy", power: 5, size: .large), Skill(name: "lightling", power: 3, size: .small)], items: [Item(name: "holybook", intro: "Spirit", price: 20, size: .small), Item(name: "cross", intro: "Belief", price: 20, size: .small)], stats: Stats(health: 100, endurance: 500),
                  bag: [Item(name: "apple", intro: "Food", price: 5, size: .small)],
                  heroLoad: 4,
-                 activeSkills: [Skill(name: "lightling", power: 3, size: .large)],
+                 activeSkills: [Skill(name: "lightling", power: 3, size: .large)]
                 ),
             
             // 3 - Follower
             Hero(heroClass: HeroClass(name: .duelist, level: 12, life: 80), attributes: Attributes(Strength: 6, Intelligence: 1, Wisdom: 9, Agility: 1, Vitality: 5, Faith: 1, Charisma: 5), skills: [Skill(name: "gun", power: 7,size: .small)/*,Skill(name: "fist", power: 6)*/], items: [Item(name: "fakeID", intro: "detect using", price: 20, size: .small)/*, Item(name: "letter", intro: "read")*/], stats: Stats(health: 100, endurance: 500),
                  bag: [Item(name: "apple", intro: "Food", price: 5, size: .small)],
                  heroLoad: 2,
-                 activeSkills: [Skill(name: "Rainy", power: 7,size: .small), Skill(name: "gun", power: 7,size: .small)],
+                 activeSkills: [Skill(name: "Rainy", power: 7,size: .small), Skill(name: "gun", power: 7,size: .small)]
                 )
         ]
         
--- JourneyOfMidnight/View/Main/HeroMainView.swift
+++ JourneyOfMidnight/View/Main/HeroMainView.swift
@@ -37,7 +37,7 @@ struct HeroMainView: View {
             switch eventState {
             case .combat:
                 EventCombat()
-//                EnemyCardSet(selectedEnemies: $selectedEnemies, showDetailSkillViewEnemi: $showDetailSkillViewEnemi, showMoreDetailEnemi: $showMoreDetailEnemi) 
+//              EnemyCardSet(selectedEnemies: $selectedEnemies, showDetailSkillViewEnemi: $showDetailSkillViewEnemi, showMoreDetailEnemi: $showMoreDetailEnemi) 
                 Spacer()
                 
             case .vendor:
